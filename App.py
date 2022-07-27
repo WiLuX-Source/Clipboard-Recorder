4909aa67d92bf84c7e4cc8ca3dd5a75239ab9624
WiLuX <61833703+WiLuX-Source@users.noreply.github.com>
2022-07-27 16:44:07 +0300

    1.0
    Pushing first release.

---
+++ App.py
@@ -0,0 +1,65 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang.builder import Builder
+from kivy.core.clipboard import Clipboard
+from kivy.config import Config
+from kivy.clock import Clock
+from KivyOnTop import register_topmost
+from kivy.core.image import Image as CoreImage
+from PIL import ImageGrab, Image
+from io import BytesIO
+from datetime import datetime
+Builder.load_file('./App.kv')
+Config.set('graphics','resizable',0)
+Config.set('input','mouse','mouse,multitouch_on_demand')
+Config.set('kivy','exit_on_escape',0)
+Config.set('graphics','height',500)
+Config.set('graphics','width',400)
+class UpperControls(GridLayout):
+    temp_clipboard = ''
+    temp_img = Image.new(mode="RGB", size=(10, 10))
+    def RecordClipboard(self,root):
+        if ImageGrab.grabclipboard() != None and list(self.temp_img.getdata()) != list(ImageGrab.grabclipboard().getdata()):
+            img = ImageGrab.grabclipboard()
+            self.temp_img = img
+            images = BytesIO()
+            img.save(images,format='png')
+            img.save(f"Images/{datetime.now().strftime('%d.%m.%Y %H-%M-%S')}.png",format='png')
+            images.seek(0)
+            self.ids.img.texture = CoreImage(images, ext='png').texture
+            self.ids.last_item.opacity = 0
+            self.ids.img.opacity = 1
+        elif Clipboard.paste() != self.temp_clipboard and Clipboard.paste() != '' and Clipboard.paste().find(self.children[1].ids.keyword.text) != -1:
+            self.ids.img.opacity = 0
+            self.ids.last_item.opacity = 1
+            self.temp_clipboard = Clipboard.paste()
+            self.ids.last_item.text = Clipboard.paste()
+            with open('History.txt', '+a') as output:
+                try:
+                    output.write("%s\n" % str(Clipboard.paste()))
+                except:
+                    output.write("%s\n" % str(Clipboard.paste().encode('UTF-8')))
+
+    def ButtonToggle(self,root):
+        if root.state == "normal":
+            root.text = "Record OFF"
+            self._Timer.cancel()
+        else:
+            root.text = "Record ON"
+            self._Timer = Clock.schedule_interval(self.RecordClipboard,1)
+
+class Controls(GridLayout):
+    def Keyword_set(self):
+        input_box = self.ids.keyword
+        input_label = self.ids.display_keyword
+        input_label.text = input_box.text
+
+class ClipboardApp(App):
+    def build(self):
+        from kivy.core.window import Window
+        self.title = "WiLuX Clipboard Recorder"
+        register_topmost(Window, self.title)
+        return UpperControls()
+
+
+ClipboardApp().run()
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Clipboard-Recorder
-Simple "Clipboard Recorder" made with Python &amp; Kivy gui.
+<div align="center">
+
+# Clipboard Recorder
+
+</div>
+
+[![Github last commit date](https://img.shields.io/github/last-commit/WiLuX-Source/Clipboard-Recorder.svg?label=Updated&logo=github&cacheSeconds=600)](https://github.com/WiLuX-Source/Clipboard-Recorder/commits)
+[![Total downloads](https://img.shields.io/github/downloads/WiLuX-Source/Clipboard-Recorder/total.svg?label=Downloads&logo=github&cacheSeconds=600)](https://github.com/WiLuX-Source/Clipboard-Recorder/releases)
+[![License](https://img.shields.io/github/license/WiLuX-Source/Clipboard-Recorder.svg?label=License&cacheSeconds=2592000)](https://github.com/WiLuX-Source/Clipboard-Recorder/blob/main/LICENSE)
+[![Discord](https://img.shields.io/discord/852591290872233984.svg?color=7289da&label=Discord&logo=discord&logoColor=white&cacheSeconds=3600)](https://discord.gg/BvT98xY2rr)
+
+[![GitHub release version](https://img.shields.io/github/v/release/WiLuX-Source/Clipboard-Recorder.svg?label=Version&logo=github&cacheSeconds=600)](https://github.com/WiLuX-Source/Clipboard-Recorder/releases)
+[![GitHub release date](https://img.shields.io/github/release-date-pre/WiLuX-Source/Clipboard-Recorder.svg?label=Released&logo=github&cacheSeconds=600)](https://github.com/WiLuX-Source/Clipboard-Recorder/releases)
+[![Github release downloads](https://img.shields.io/github/downloads-pre/WiLuX-Source/Clipboard-Recorder/latest/total.svg?label=Downloads&logo=github&cacheSeconds=600)](https://github.com/WiLuX-Source/Clipboard-Recorder/releases)
+
+---
+
+## Description
+
+Simple "Clipboard Recorder" made with Python & Kivy gui.
+
+---
+
+### Features
+
+- Record Text
+- Record Images (Only copied ones.)
+- Record Text with Keyword
+- "Always On Top" Window
+
+---
+
+### Installation
+
+- ```console
+   1. cd [App Location]
+   2. python3 -m pip install -r requirements.txt
+   3. python3 App.py
+   ```
+- Create a folder called "Images"
+
+---
+
+### Compatibility
+
+Clipboard Recorder supports Windows, MacOS and Linux operating systems, but it can work anywhere where you can obtain working Python3 runtime.
+
+---
+
+### FAQ
